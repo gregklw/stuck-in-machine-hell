282288b1401b0d1dc94e19ed3666b882c0752f6e
DESKTOP-KR1T0KE\Anon <gregoryklwong@gmail.com>
2024-11-07 01:17:04 -0500

    refactor: change weapon powerups to use particle weapon scripts instead

--- Assets/Items/Scripts/PowerupTypes/WeaponPowerup.cs
+++ Assets/Items/Scripts/PowerupTypes/WeaponPowerup.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class WeaponPowerup : Powerup
 {
-    [SerializeField] private Weapon _weaponPrefab;
-    public Weapon WeaponPrefab => _weaponPrefab;
+    [SerializeField] private PlayerParticleWeapon _weaponPrefab;
+    public PlayerParticleWeapon WeaponPrefab => _weaponPrefab;
     public override void ActivatePowerup(Player player)
     {
         //consider destroying weapon argument or prevent instantiation when weapon already exists
