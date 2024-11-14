caafa698fbbb23f97cce4c95554ecbf359912a9a
gwongrp4k <gwong@realprogramming.com>
2024-11-14 14:36:00 -0500

    chore: remove character skin data references and files

--- Assets/General/Scripts/CharacterSkinData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CharacterSkinData", menuName = "ScriptableObjects/CharacterSkinData")]
-public class CharacterSkinData : ScriptableObject
-{
-    public Sprite CharacterSprite;
-    public RuntimeAnimatorController DeathExplosionAnimation;
-}
